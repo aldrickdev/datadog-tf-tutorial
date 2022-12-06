d41ed0d93221455464e1cf9e7ad4670197da470c
aldrick.castro <aldrick.castro@datadoghq.com>
2022-12-05 21:45:06 -0500

    Added the create monitor code

---
+++ 0-create-monitor/main.tf
@@ -0,0 +1,42 @@
+# Import the Datadog Provider
+terraform {
+  required_providers {
+    datadog = {
+      source = "DataDog/datadog"
+    }
+  }
+}
+
+# Define the variables
+variable "datadog_api_key" {
+  description = "Datadog API Key"
+}
+
+variable "datadog_app_key" {
+  description = "Datadog App Key"
+}
+
+# Configure the Datadog Provider
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+}
+
+# Define the Monitor
+resource "datadog_monitor" "foo" {
+  name               = "Name for monitor foo"
+  type               = "metric alert"
+  message            = "Monitor triggered. Notify: @hipchat-channel"
+  escalation_message = "Escalation message @pagerduty"
+
+  query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo} by {host} > 4"
+
+  monitor_thresholds {
+    warning  = 2
+    critical = 4
+  }
+
+  include_tags = true
+
+  tags = ["foo:bar", "team:fooBar"]
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # datadog-tf-tutorial
-This a supportive repository for the Datadog Terraform Tutorial that has code examples that can be used 
+
+This a supportive repository for the Datadog Terraform Tutorial that has code examples that can be used to follow along with the tutorial.
